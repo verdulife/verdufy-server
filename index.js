bf0c6cf29dd44503d04ac57c329f57ccd08f9fb0
verdulife <verdu@live.com>
2022-03-14 17:48:58 +0100

    inital 2

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
-import { ApolloServer, gql } from "apollo-server";
+import { ApolloServer, gql } from "apollo-server-express";
+import { ApolloServerPluginDrainHttpServer } from "apollo-server-core";
+import http from "http";
+import express from "express";
+import cors from "cors";
 import { search, stream } from 'play-dl';
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+const httpServer = http.createServer(app);
 
 const typeDefs = gql`
   type Song {
@@ -45,7 +54,17 @@ const resolvers = {
   }
 };
 
-const server = new ApolloServer({ typeDefs, resolvers });
-const { url } = await server.listen();
+const startApolloServer = async (app, httpServer) => {
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+  });
 
-console.log(`🚀 Server ready at ${url}`);
+  await server.start();
+  server.applyMiddleware({ app });
+}
+
+startApolloServer(app, httpServer);
+
+export default httpServer;
